78968ced97a3f85004e10a9c7e6e6c76e1547c16
myhadas <hadassaharmel4@gmail.com>
2023-04-19 02:09:17 +0300

    Authentication with Oauth

--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,17 @@
 using AppServices;
+using Microsoft.AspNetCore.Authentication;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddAuthentication().AddGoogle(googleOptions =>
+{
+    googleOptions.ClientId = builder.Configuration.GetSection("googleAuthSettings").GetValue<string>("ClientId");
+    googleOptions.ClientSecret = builder.Configuration.GetSection("GoogleAuthSettings").GetValue<string>("ClientSecret");
+});
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddAppServices();
+
 
 var app = builder.Build();
 
